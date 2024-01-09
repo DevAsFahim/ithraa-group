175a36507a4df6ee6f0a1cc95a66c83b2a03438a
DevAsFahim <108362901+DevAsFahim@users.noreply.github.com>
2024-01-09 15:37:29 +0600

    home page added

---
+++ src/javascript/app.js
@@ -0,0 +1,27 @@
+const trigger = document.querySelector("#modal-trigger");
+const modal = document.querySelector("#modal");
+const close = document.querySelector(".close-modal");
+
+trigger.addEventListener("click", () => {
+  modal.style.display = "block";
+});
+
+close.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+window.addEventListener("click", (event) => {
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+});
+
+// ----------------
+// hero slider
+
+// $(document).ready(function(){
+//   $('.hero-slider').slick({
+//     // setting-name: setting-value
+//   });
+// });
+
